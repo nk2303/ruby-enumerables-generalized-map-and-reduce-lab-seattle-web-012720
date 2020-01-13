2e7a734330071a63002acfa47cac4ac0fd4eb799
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 23:17:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new_arr = []
+  array.length.times do |index|
+    new_arr.push(yield array[index])
+  end
+  new_arr
+end
+#expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+
+def reduce(array, sp = nil)
+  if sp
+    sum = sp
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+  end
+  sum
+end
+# source_array = [ false, nil, nil, nil, true]
+# expect(reduce(source_array){|memo, n| memo || n}).to eq(true)
